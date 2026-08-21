954dc30a3a0d41b877413c640b90df7941fe9c7b
Beryan Albeik <beryanalbeik@gmail.com>
2026-08-21 14:20:12 +0300

    styling problem fix fix bitmiyor

--- components/tetris/StartButton.jsx
+++ components/tetris/StartButton.jsx
@@ -1,11 +1,35 @@
-import { StyledStartButton } from "@/components/tetris/styles/StyledStartButton";
+import { Pressable } from "react-native";
 
 function StartButton({ callback , text}) {
   return (
-    <StyledStartButton onClick={callback}> {/*tuşa basılınca start game çalışacak*/}
-        {text}
-    </StyledStartButton>
+    <Pressable onPress={callback} style={({ pressed }) => [
+        styles.button,
+        pressed && styles.buttonPressed // Tıklama efekti (opaklık)
+      ]}> {/*tuşa basılınca start game çalışacak*/}
+        <Text style={styles.text}>{text}</Text>
+    </Pressable>
   );
 }
 
 export default StartButton;
+
+const styles = StyleSheet.create({
+  button: {
+    marginBottom: 20,
+    padding: 20,
+    minHeight: 30,
+    width: '100%',
+    borderRadius: 20,
+    backgroundColor: '#cc6c8c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonPressed: {
+    opacity: 0.8,
+  },
+  text: {
+    color: '#ffffff',
+    fontFamily: 'monospace',
+    fontSize: 16,
+  },
+})
